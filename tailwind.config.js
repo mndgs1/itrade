a41b4fba3c943b930cddacd5caaf88bda881ba18
mndgs1 <mindaugasbankauskas@gmail.com>
2023-08-24 14:48:56 +0200

    Installed Vite & Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 // const colors = require('tailwindcss/colors')
 
 module.exports = {
-  content: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["index.html", "./src/**/*.{html,js}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
